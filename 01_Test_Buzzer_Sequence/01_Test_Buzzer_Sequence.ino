e376bbda025bace157376698ad3bfd799df2ef73
Nathan <nathanael.esnault@gmail.com>
2022-03-30 18:29:43 +1300

    Update 01_Test_Buzzer_Sequence.ino

--- 01_Test_Buzzer_Sequence/01_Test_Buzzer_Sequence.ino
+++ 01_Test_Buzzer_Sequence/01_Test_Buzzer_Sequence.ino
@@ -2,31 +2,38 @@
 #include <CuteBuzzerSounds.h>
 
 #define BUZZER_PIN 8
+#define NBR_LOOPS 4
 
 int loopCounter = 0;
 
 void setup() 
 {
-  cute.init(BUZZER_PIN);
+  
 
 // Declare pins
 
+cute.init(BUZZER_PIN);
+
 // Console start
+  Serial.begin(9600);
+  while(!Serial) // Wait here until the connection with the computer has been established
+
+
 
 // Set up starts
-cute.play(S_OHOOH2        ); // Done for colorimeter test  AND Set up start
+cute.play(S_OHOOH2);
 
 delay(2000);
 
 // Set up is done
-cute.play(S_MODE3         );
+cute.play(S_MODE3);
 
 
 delay(2000);
 
 
  //start in manual mode
- cute.play(S_HAPPY         ); // Manual mode start
+ cute.play(S_HAPPY); // Manual mode start
 
   
 }
@@ -53,45 +60,49 @@ void loop()
 {
 
 
+switch( loopCounter )
+{
+
+  cute.play(S_BUTTON_PUSHED ); // Start of a new step
+
+  
+  case 1:
+      cute.play(S_CONNECTION  ); // button pressed (Start-repeat-Continue)
+      cute.play(S_HAPPY         ); // Manual mode start
+      break;
+  case 2:
+      cute.play(S_HAPPY_SHORT   ); // Auto mode start
+      break;
+  case 3:
+      cute.play(S_MODE3         ); // Ready for colorimeter test AND Set up done
+      delay(1000);
+      cute.play(S_MODE3         ); // Ready for colorimeter test AND Set up done
+      delay(1000);
+      cute.play(S_MODE3         ); // Ready for colorimeter test AND Set up done
+      delay(1000);
+      cute.play(S_OHOOH2        ); // Done for colorimeter test  AND Set up start
+      break;
+   case 4:
+    cute.play(S_SAD           ); // Abort button presed
+  break;
+  default:
+      cute.play(S_SAD           ); // Abort button presed
+      break;
+}
+
+
+// With ternary operator
+  (loopCounter >= NBR_LOOPS-1) ?  loopCounter = 0 : loopCounter++;
+  delay(3000);
+
+  if (loopCounter == 0)
+  {
+    delay(5000);
+    Serial.println("------------New cycle--------------");
+  }
+
 
 // All the following are blocking calls [7]
   
-  cute.play(S_CONNECTION  ); // button pressed (Start-repeat-Continue)
-delay(2000);
-//cute.play(S_DISCONNECTION )  ; // 
-//delay(2000);
-cute.play(S_BUTTON_PUSHED ); // Start of a new step
-delay(2000);
-//cute.play(S_MODE1         );
-//delay(2000);
-//cute.play(S_MODE2         );
-//delay(2000);
-cute.play(S_MODE3         ); // Ready for colorimeter test AND Set up done
-delay(2000);
-//cute.play(S_SURPRISE      ); 
-//delay(2000);
-//cute.play(S_OHOOH         );
-//delay(2000);
-cute.play(S_OHOOH2        ); // Done for colorimeter test  AND Set up start
-delay(2000);
-//cute.play(S_CUDDLY        );
-//delay(2000);
-//cute.play(S_SLEEPING      );
-//delay(2000);
-cute.play(S_HAPPY         ); // Manual mode start
-delay(2000);
-//cute.play(S_SUPER_HAPPY   );
-//delay(2000);
-cute.play(S_HAPPY_SHORT   ); // Auto mode start
-delay(2000);
-cute.play(S_SAD           ); // Abort button presed 
-delay(2000);
-cute.play(S_CONFUSED      );
-delay(2000);
-//cute.play(S_FART1         );
-//delay(2000);
-//cute.play(S_FART2         );
-//delay(2000);
-//cute.play(S_FART3         );
-//delay(2000);
+ 
 }
