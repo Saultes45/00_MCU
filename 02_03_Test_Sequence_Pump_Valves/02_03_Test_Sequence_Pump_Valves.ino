c481ac3e2b9acd4388f712885e2d58d25c0a1e2b
Nathan <nathanael.esnault@gmail.com>
2022-04-01 10:30:08 +1300

    Update 02_03_Test_Sequence_Pump_Valves.ino

--- 02_03_Test_Sequence_Pump_Valves/02_03_Test_Sequence_Pump_Valves.ino
+++ 02_03_Test_Sequence_Pump_Valves/02_03_Test_Sequence_Pump_Valves.ino
@@ -59,10 +59,10 @@ const uint8_t PIN_MOTOR_DIR    = 10; // Digital output 5V
 const uint8_t PIN_MOTOR_ENA    = 12; // Digital output 5V, manual logic
 
 
-const uint8_t PIN_MOTOR_CFG_0   = 7; // Digital output 5V - Doesnt matter
+const uint8_t PIN_MOTOR_CFG_0   = 7; // Digital output 5V - Doesn't matter
 const uint8_t PIN_MOTOR_CFG_1   = 4; // Digital output 5V
 const uint8_t PIN_MOTOR_CFG_2   = 5; // Digital output 5V
-const uint8_t PIN_MOTOR_CFG_3   = 6; // Digital output 5V - Doesnt matter
+const uint8_t PIN_MOTOR_CFG_3   = 6; // Digital output 5V - Doesn't matter
 
 const uint8_t uStepFactor = 4; // set up by the MCU on the stepper driver
 const uint8_t pulsesPerRevolution = 200; // Dictated by the HW only
@@ -82,9 +82,9 @@ const uint16_t targetAccel = personalMaxAccel; //in pulses/sec so theo NOT depen
 #define PUMP_DISABLE  HIGH
 
 #define MUX_ADDRSS 0x20 
-#define NBR_FUNCTIONS 6 // Also defines how many valves
+#define NBR_FUNCTIONS 6 // Also defines how many valves sequences
 //                                        F1 - F2 - F3 - F4 - F5 - F6
-uint8_t valveMuxArray[NBR_FUNCTIONS] = {0x042,0xD2,0xA4,0xA8,0x90,0x00};
+const uint8_t valveMuxArray[NBR_FUNCTIONS] = {0x42,0xD2,0xA4,0xA8,0x90,0x00};
 
 
 // Valves INDX
@@ -108,18 +108,21 @@ uint8_t valveLoop           = 7;
 uint8_t cnt_functions = 0;
 
 // Console
-//-----------
+//--------
 #define SERIAL_DEBUG // Uncomment to let the board print debug statements through the debug port (USB)
 #define SERIAL_BAUDS 9600
 
 // Buzzer
+//-------
 //#define USE_BUZZER
 #define BUZZER_PIN 8
 
+//OLED
+//----
 #define USE_OLED
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET    -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 #ifdef USE_OLED
   Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
@@ -148,6 +151,11 @@ AccelStepper stepper(AccelStepper::DRIVER, PIN_MOTOR_STEP, PIN_MOTOR_DIR);
 // I2C 8-pin mux for valve driver
 PCF8574 VALVE_MUX(MUX_ADDRSS);
 
+// Sequencing
+//------------
+bool needPump = false;
+bool nextFunction = false;
+
 
 // -------------------------- Function declaration [2]----------------
 void      displayStepperSettings  (void);
@@ -156,30 +164,27 @@ void      setupValves             (void);
 void      setupStepper            (void);
 void      setupBuzzer             (void);
 void      setupOLED               (void);
+void      printFunctionOLED       (void);
 
 
 
 
 
 // -------------------------- SetUp --------------------------
-void setup()
-{
+void setup() {
 
   pinSetUp();
-  
-  #ifdef USE_BUZZER
-  
-    setupBuzzer();
 
-    // Set up starts
-    cute.play(S_OHOOH2);
-    
+  #ifdef USE_BUZZER
+  setupBuzzer();
+  // Set up starts
+  cute.play(S_OHOOH2);
   #endif
 
   #ifdef SERIAL_DEBUG
   Serial.begin(SERIAL_BAUDS);
-  while(!Serial) // Wait here until the connection with the computer has been established
-  #endif
+  while (!Serial) // Wait here until the connection with the computer has been established
+    #endif
 
   delay(1000);
 
@@ -187,219 +192,252 @@ void setup()
   printDebugln("Starting a new session");
   printDebugln("Welcome to the Stepper+Valve test");
 
-  
-//  printDebug("Initialisation...");
-//  printDebugln("done");
+  //  printDebug("Initialisation...");
+  //  printDebugln("done");
 
- 
-setupOLED();
-setupValves();
-setupStepper();
+  setupOLED();
+  setupValves();
+  setupStepper();
 
-
-
+  nextFunction = true; // to start the function counter
 
   printDebugln("End of the setup");
   printDebugln("---------------------------------"); // Indicates the end of the setup
 
-    #ifdef USE_BUZZER
+  #ifdef USE_BUZZER
 
-    // Set up is done
-    cute.play(S_MODE3);
-    
+  // Set up is done
+  cute.play(S_MODE3);
+
   #endif
-  
+
 }
 
 
 // -------------------------- Loop --------------------------
-void loop()
-{
+void loop() {
 
-// Write a value to the whole pins at once
-  printDebug("counter F");
-  printDebugln(cnt_functions+1);
-
-  VALVE_MUX.write8(valveMuxArray[cnt_functions]);
-  printDebugln(valveMuxArray[cnt_functions]);
-
-if (needPump)
-{
-  if (stepper.distanceToGo() == 0)
+  // Check if we have finished the previous function before we can go to the next
+  //------------------------------------------------------------------------------
+  if (nextFunction) 
   {
-    // go to the next function
-    nextFunction = true;
-  }
-}
-switch (cnt_functions)
-{
-    case 1: 
-        break;
-    case 2: 
-        break;
-    default: // code to be executed if n doesn't match any cases
-}
+    nextFunction = false; //reset the flag
 
- 
-  
-if (nextFunction)
-{
+    //Debug wait
+    delay(5000);
+
     // With ternary operator
-  (cnt_functions >= NBR_FUNCTIONS-1) ?  cnt_functions = 0 : cnt_functions++;
-  delay(1000);
+    (cnt_functions >= NBR_FUNCTIONS) ? cnt_functions = 1: cnt_functions++;
 
-  if (cnt_functions == 0)
+    if (cnt_functions == 1) 
+    { // if this is a new cycle
+      delay(2000); // wait a bit
+
+      printDebugln("------------New cycle--------------");
+ 
+    } // new cycle
+
+    printFunctionOLED();
+
+    printDebug("Executing function F");
+    printDebugln(cnt_functions);
+
+    // Change the all MUX pin at once
+    printDebug("Changing valves state to ");
+    printDebug(valveMuxArray[cnt_functions-1]);
+    printDebug("...");
+    VALVE_MUX.write8(valveMuxArray[cnt_functions-1]);
+    printDebugln("done");
+
+    // Now that the valves are in the correct position, let's pump!
+    switch (cnt_functions) {
+      case 0:
+        // You should enter here only once, when you have finished set up
+        needPump = false; // let's prime, but not yet
+        nextFunction = true; // call for immediate next function
+      case 1:
+        // Clean A side
+        needPump = true; // accel + max speed 
+        nextFunction = false; // set after the pump movement
+
+        printDebug("Enabling the motor...");
+        // Enable the motor (inverse logic)
+        digitalWrite(PIN_MOTOR_ENA, PUMP_ENABLE); // remove for more precision
+        printDebugln("done");
+
+        // Wait for the locking in place
+        delay(500);
+
+
+        printDebug("Starting the motor to ");
+        printDebug(-1 * target_nbr_revolutions * pulsesPerRevolution * uStepFactor);
+        printDebug(" ... ");
+        // Start the pump TODO: optimise
+        // move() is long relative
+        // moveTo() is long absolute
+        stepper.move(-1 * target_nbr_revolutions * pulsesPerRevolution * uStepFactor);
+        printDebugln("done");
+
+        break;
+
+      case 2:
+        needPump = false; // let's prime, but not yet
+        nextFunction = true; // call for immediate next function
+        break;
+      case 3:
+        needPump = false; // let's prime, but not yet
+        nextFunction = true; // call for immediate next function
+        break;
+      case 4:
+        needPump = false; // let's prime, but not yet
+        nextFunction = true; // call for immediate next function
+        break;
+      case 5:
+        needPump = false; // let's prime, but not yet
+        nextFunction = true; // call for immediate next function
+        break;
+      case 6:
+        needPump = false; // let's prime, but not yet
+        nextFunction = true; // call for immediate next function
+        break;
+      default: // code to be executed if n doesn't match any cases
+        break;
+      }
+
+
+  } // nextFunction
+  else // we keep on the same function, we might be waiting for the pump to finish
   {
-    delay(2000);
-    printDebugln("------------New cycle--------------");
+    if (needPump) {
+
+      stepper.run(); // non blocking
+
+      if (stepper.distanceToGo() == 0) { // check if the pump movement is finished
+        nextFunction = true; // go to the next function
+        needPump = false;
+
+        printDebugln("Pump movement done!");
+
+        printDebug("Disabling the motor...");
+        // Disable the motor (inverse logic)
+        digitalWrite(PIN_MOTOR_ENA, PUMP_DISABLE); // remove for more precision
+        printDebugln("done");
+
+        // TODO: display how much time it took to perform the movement
+
+        #ifdef USE_BUZZER
+        // The motor stops
+        cute.play(S_MODE3);
+        #endif
+      }
+    }
   }
-}
 
 
-
-  // If at the end of travel go to the other end
-  if (stepper.distanceToGo() == 0)
-  {
-
-    // Disable the motor (inverse logic)
-    digitalWrite(PIN_MOTOR_ENA,PUMP_DISABLE);
-
-    #ifdef USE_BUZZER
-    // Set up is done
-    cute.play(S_MODE3);
-  #endif
-    
-    delay(3000);
-
-      stepper.moveTo(-1 * target_nbr_revolutions * pulsesPerRevolution * uStepFactor);
-  
-  }
-
-  stepper.run(); // non blocking
-
-}
+} // END OF LOOP
 
 
 
 
 
 //******************************************************************************************
-void      pinSetUp                (void)
-{
+void pinSetUp(void) {
 
-//  // initialize digital pin LED_BUILTIN as an output.
-//  pinMode(LED_BUILTIN, OUTPUT);
-  
+  //  // initialize digital pin LED_BUILTIN as an output.
+  //  pinMode(LED_BUILTIN, OUTPUT);
+
   // initialize digital pin PIN_MOTOR_ENA as an output.
   pinMode(PIN_MOTOR_ENA, OUTPUT);
 
-    // Disable the motor (logic inverse)
-  digitalWrite(PIN_MOTOR_ENA,PUMP_DISABLE);
+  // Disable the motor (logic inverse)
+  digitalWrite(PIN_MOTOR_ENA, PUMP_DISABLE);
 
   // set motor configuration
-//+--------------+-----------+----------------------------+---------------------------------------+
-//| Config Index | MCU or SB |            Value           |                 Effect                |
-//+--------------+-----------+----------------------------+---------------------------------------+
-//|     CFG_0    |    JP1    | GND                        | Chopper off time = 140                |
-//+--------------+-----------+----------------------------+---------------------------------------+
-//|     CFG_1    |    MCU    | HiZ (INPUT)                |                                       |
-//+--------------+-----------+----------------------------+  4/256 StealthChop                    |
-//|     CFG_2    |    MCU    | VCC_IO                     |                                       |
-//+--------------+-----------+----------------------------+---------------------------------------+
-//|     CFG_3    |    JP2    | HiZ                        | Mode of current AIN: external sense V |
-//+--------------+-----------+----------------------------+---------------------------------------+
-//|     CFG_4    |     SB    | GND                        | Chopper hysteresis = 5                |
-//+--------------+-----------+----------------------------+---------------------------------------+
-//|     CFG_5    |     SB    | VCC_IO                     | Chopper blank Time = 24               |
-//+--------------+-----------+----------------------------+---------------------------------------+
-//|     CFG_6    |    MCU    | Changes from VCC_IO to GND | Disable/Enable the stepper            |
-//+--------------+-----------+----------------------------+---------------------------------------+
-//  
+  //+--------------+-----------+----------------------------+---------------------------------------+
+  //| Config Index | MCU or SB |            Value           |                 Effect                |
+  //+--------------+-----------+----------------------------+---------------------------------------+
+  //|     CFG_0    |    JP1    | GND                        | Chopper off time = 140                |
+  //+--------------+-----------+----------------------------+---------------------------------------+
+  //|     CFG_1    |    MCU    | HiZ (INPUT)                |                                       |
+  //+--------------+-----------+----------------------------+  4/256 StealthChop                    |
+  //|     CFG_2    |    MCU    | VCC_IO                     |                                       |
+  //+--------------+-----------+----------------------------+---------------------------------------+
+  //|     CFG_3    |    JP2    | HiZ                        | Mode of current AIN: external sense V |
+  //+--------------+-----------+----------------------------+---------------------------------------+
+  //|     CFG_4    |     SB    | GND                        | Chopper hysteresis = 5                |
+  //+--------------+-----------+----------------------------+---------------------------------------+
+  //|     CFG_5    |     SB    | VCC_IO                     | Chopper blank Time = 24               |
+  //+--------------+-----------+----------------------------+---------------------------------------+
+  //|     CFG_6    |    MCU    | Changes from VCC_IO to GND | Disable/Enable the stepper            |
+  //+--------------+-----------+----------------------------+---------------------------------------+
+  //  
 
   pinMode(PIN_MOTOR_CFG_1, INPUT); // for HiZ
   pinMode(PIN_MOTOR_CFG_2, OUTPUT);
-  
-//  digitalWrite(PIN_MOTOR_CFG_1,LOW); // 4/256 StealthChop
-  digitalWrite(PIN_MOTOR_CFG_2,HIGH); // 4/256 StealthChop
 
-
+  //  digitalWrite(PIN_MOTOR_CFG_1,LOW); // 4/256 StealthChop
+  digitalWrite(PIN_MOTOR_CFG_2, HIGH); // 4/256 StealthChop
 
 } // END OF FUNCTION
 
-
-
-
 //******************************************************************************************
-void setupValves(void)
-{
+void setupValves(void) {
 
-   printDebug("PCF8574_LIB_VERSION:\t");
+  printDebug("PCF8574_LIB_VERSION:\t");
   printDebugln(PCF8574_LIB_VERSION);
 
-// Valves
-//-------
+  // Valves
+  //-------
   printDebug("Trying to connect to PCF8574 at I2C address ");
   printDebug(MUX_ADDRSS);
   printDebug("(dec)...");
-  if (!VALVE_MUX.begin())
-  {
+  if (!VALVE_MUX.begin()) {
     printDebugln("PCF8574 could not be initialized");
   }
-  if (!VALVE_MUX.isConnected())
-  {
+  if (!VALVE_MUX.isConnected()) {
     printDebugln("NOT connected");
-  }
-  else
-  {
+  } else {
     printDebugln("connected");
   }
 
-  
-}// END OF THE FUNCTION
+} // END OF THE FUNCTION
 
 //******************************************************************************************
-void setupStepper(void)
-{
+void setupStepper(void) {
 
-// Stepper set MAX limit values
-//------------------------------
-
+  // Stepper set MAX limit values
+  //------------------------------
 
   // To calculate max acceleration, max speed must be set first
-  
+
   printDebug("Setting max SPEED to: ");
   printDebugln(personalMaxSpeed * uStepFactor);
   stepper.setMaxSpeed(personalMaxSpeed * uStepFactor);
-  
+
   printDebug("Setting max ACCEL to: ");
   printDebugln(personalMaxAccel * uStepFactor);
   stepper.setMaxSpeed(personalMaxAccel * uStepFactor);
-  
-// Stepper accel + pos target
-//----------------------------
+
+  // Stepper accel + pos target
+  //----------------------------
   stepper.setAcceleration(targetAccel * uStepFactor);
   // do NOT set the speed with setSpeed, it is ONLY for
   stepper.moveTo(target_nbr_revolutions * pulsesPerRevolution * uStepFactor);
 
-
-// Stepper display settings
-//-------------------------
+  // Stepper display settings
+  //-------------------------
   displayStepperSettings();
 
   // Enable the motor (logic inverse)
-  digitalWrite(PIN_MOTOR_ENA,PUMP_ENABLE);
-  
-}// END OF THE FUNCTION
+  digitalWrite(PIN_MOTOR_ENA, PUMP_ENABLE);
 
-
+} // END OF THE FUNCTION
 
 //******************************************************************************************
-void displayStepperSettings(void)
-{
+void displayStepperSettings(void) {
   // Display settings to UART
   //-------------------------
   printDebugln("---------------------------------");
-
 
   printDebug("Current full steps per rotation: ");
   printDebugln(pulsesPerRevolution);
@@ -407,78 +445,91 @@ void displayStepperSettings(void)
   printDebugln(pulsesPerRevolution * uStepFactor);
   printDebug("Current micro-steping factor: ");
   printDebugln(uStepFactor);
-    printDebug("Current interpolation factor: ");
+  printDebug("Current interpolation factor: ");
   printDebugln(interpolationFactor);
-    printDebug("Current mode: ");
+  printDebug("Current mode: ");
   printDebugln(driverMode);
 
   Serial.println("---------------------------------");
 
-//MAX
+  //MAX
   printDebug("Current max speed: ");
   printDebugln(stepper.maxSpeed()); // Returns the most recent speed in steps per second
 
   printDebug("Current max accel: ");
   printDebugln("No function to read back MAX set acceleration");
 
-// Current targets
+  // Current targets
   printDebug("Current target speed: ");
   printDebugln(stepper.speed()); // The most recently set speed
 
   printDebug("Current target accel: ");
   printDebugln("No function to read back TARGET set acceleration");
 
-// Position target
- printDebug("Distance to go: ");
+  // Position target
+  printDebug("Distance to go: ");
   printDebugln(stepper.distanceToGo());
 
-   printDebug("Target position: ");
+  printDebug("Target position: ");
   printDebugln(stepper.targetPosition());
 
-     printDebug("Current position: ");
+  printDebug("Current position: ");
   printDebugln(stepper.currentPosition());
 
-
-
-}// END OF THE FUNCTION
-
-
-
+} // END OF THE FUNCTION
 
 //******************************************************************************************
-void setupBuzzer(void)
-{
+void setupBuzzer(void) {
   cute.init(BUZZER_PIN);
-  
-}// END OF THE FUNCTION
+
+} // END OF THE FUNCTION
 
 //******************************************************************************************
-void setupOLED(void)
-{
-    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+void setupOLED(void) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     printDebugln(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;); // Don't proceed, loop forever
   }
 
   // Clear the buffer
   display.clearDisplay();
 
-
   // Follow (2022-02-22)
   //https://randomnerdtutorials.com/guide-for-oled-display-with-arduino/
 
-    // Display text “Valve Test”
+  // Display text “Valve Test”
   //------------------------------
   display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(5, 20);
   // Display static text
   display.println("Valve Test");
-  display.display(); 
+  display.display();
+
+} // END OF THE FUNCTION
+
+//******************************************************************************************
+void printFunctionOLED(void) {
+
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Follow (2022-02-22)
+  //https://randomnerdtutorials.com/guide-for-oled-display-with-arduino/
+
+  // Display text "F-"
+  //------------------
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(5, 20);
+  // Display static text
+  display.print("F-");
+  display.println(cnt_functions);
   
-}// END OF THE FUNCTION
+  display.display();
 
-
+} // END OF THE FUNCTION
 
 
 // END OF THE FILE
