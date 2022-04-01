47700d629a547a61dcf9c72412cc1754baf23e1f
Nathan <nathanael.esnault@gmail.com>
2022-04-01 18:00:05 +1300

    Transfer data to separate header file

--- 02_03_Test_Sequence_Pump_Valves/02_03_Test_Sequence_Pump_Valves.ino
+++ 02_03_Test_Sequence_Pump_Valves/02_03_Test_Sequence_Pump_Valves.ino
@@ -41,6 +41,7 @@
 
 // for pump
 #include <AccelStepper.h>
+#include "StepperPeristaltic.h" // personal
 
 
 // for buzzer
@@ -76,49 +77,8 @@ const uint8_t LED_BLU_PIN   = 45; // Digital output 5V - PWM capable
 // Stepper
 //--------
 
-// Enable logic (active low)
-#define PUMP_ENABLE   LOW
-#define PUMP_DISABLE  HIGH
-
-// Movement pins
-const uint8_t PIN_MOTOR_STEP   = 11; // Digital output 5V
-const uint8_t PIN_MOTOR_DIR    = 10; // Digital output 5V
-const uint8_t PIN_MOTOR_ENA    = 12; // Digital output 5V, manual logic
-
-// Param
-const uint8_t PIN_MOTOR_CFG_0   = 7; // Digital output 5V - Doesn't matter
-const uint8_t PIN_MOTOR_CFG_1   = 4; // Digital output 5V
-const uint8_t PIN_MOTOR_CFG_2   = 5; // Digital output 5V
-const uint8_t PIN_MOTOR_CFG_3   = 6; // Digital output 5V - Doesn't matter
-
-// To calculate position
-const uint8_t uStepFactor = 4; // set up by the MCU on the stepper driver
-const uint8_t pulsesPerRevolution = 200; // Dictated by the HW only
-
-const int interpolationFactor = 256;
-String    driverMode          = "StealthChop";
-
-// Personal MAX limits
-const uint16_t personalMaxAccel = 1300; //in [pulses/sec^2] so theo NOT dependent on uStepFactor
-const uint16_t personalMaxSpeed = 1300; //in [pulses/sec] so theo NOT dependent on uStepFactor
-
-// dose/clean pump parameters
-// REMOVE THE NEXT LINE
-const uint16_t targetAccel = personalMaxAccel; //in pulses/sec^2 so theo NOT dependent on uStepFactor
-//const uint8_t targetSpeed = 100; //in pulses/sec so theo NOT dependent on uStepFactor
-
-//const uint16_t cleanSpeed = personalMaxSpeed; // Do not use, in [pulses/sec]
-const uint16_t cleanAccel = personalMaxAccel; // in [pulses/sec^2]
-
-//const uint16_t mixSpeed = personalMaxSpeed; // Do not use, in [pulses/sec]
-const uint16_t mixAccel = (uint16_t)((float)personalMaxAccel * 2.0/3.0); // in [pulses/sec^2]
-
-
-const uint16_t doseSpeed = 10; //  in [pulses/sec]
-//const uint16_t doseAccel = 150; // Do not use, in [pulses/sec^2]
-
+// in StepperPeristaltic.h now
 const uint16_t target_nbr_revolutions[NBR_FUNCTIONS] = {60,80,1,1,3,0};
-
 
 
 
