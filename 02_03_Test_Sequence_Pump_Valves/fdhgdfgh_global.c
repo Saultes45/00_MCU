4a2cfca83d9b0f209b4213f21d06b335b1e73c99
Nathan <nathanael.esnault@gmail.com>
2022-04-03 00:58:12 +1300

    Create fdhgdfgh_global.c

---
+++ 02_03_Test_Sequence_Pump_Valves/fdhgdfgh_global.c
@@ -0,0 +1,39 @@
+/* ========================================
+*
+* Copyright University of Auckland Ltd, 2022
+* All Rights Reserved
+* UNPUBLISHED, LICENSED SOFTWARE.
+*
+* Metadata
+* Written by    : Nathanaël Esnault
+* Verified by   : Nathanaël Esnault
+* Creation date : 2022-04-01
+* Version       : 0.1 (finished on 2021-..-..)
+* Modifications :
+* Known bugs    :
+*
+*
+* Possible Improvements
+*
+*
+* Notes
+*
+*
+* Ressources (Boards + Libraries Manager)
+* Arduino Mega 2556
+*
+* TODO
+*
+*
+* PUMP = stepper = pump = peristaltic 
+* ========================================
+*/
+
+// -------------------------- Includes[1] --------------------------
+//#include "global.h"
+
+
+// -------------------------- function definition[] --------------------------
+
+
+// END OF THE FILE
