fa72779f3da5cd2f5a42246bb05987f4b173026a
Nathan <nathanael.esnault@gmail.com>
2022-04-03 10:27:28 +1200

    Create signalBuzzer.h

---
+++ 02_03_Test_Sequence_Pump_Valves/signalBuzzer.h
@@ -0,0 +1,53 @@
+/* ========================================
+*
+* Copyright University of Auckland Ltd, 2022
+* All Rights Reserved
+* UNPUBLISHED, LICENSED SOFTWARE.
+*
+* Metadata
+* Written by    : Nathanaël Esnault
+* Verified by   : Nathanaël Esnault
+* Creation date : 2022-04-01
+* Version       : 0.1 (finished on 2021-..-..)
+* Modifications :
+* Known bugs    :
+*
+*
+* Possible Improvements
+*
+*
+* Notes
+*
+*
+* Ressources (Boards + Libraries Manager)
+* Arduino Mega 2556
+*
+* TODO
+*
+*
+* PUMP = stepper = pump = peristaltic 
+* ========================================
+*/
+
+// -------------------------- Includes [1] --------------------------
+
+// Library of buzzer sounds
+#include <CuteBuzzerSounds.h> 
+
+// -------------------------- Macros [0] --------------------------
+
+
+
+// -------------------------- Defines and Const []--------------------------
+//#define USE_BUZZER
+#define BUZZER_PIN 8
+
+
+
+
+// -------------------------- Global variables []----------------
+
+
+
+
+// END OF THE FILE
