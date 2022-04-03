e611db0e7600ef7c3addfb75f05e20e35c7b4323
Nathan <nathanael.esnault@gmail.com>
2022-04-03 15:29:59 +1200

    Create taosColorimeter.h

---
+++ 02_03_Test_Sequence_Pump_Valves/taosColorimeter.h
@@ -0,0 +1,59 @@
+/* ========================================
+*
+* Copyright University of Auckland Ltd, 2022
+* All Rights Reserved
+* UNPUBLISHED, LICENSED SOFTWARE.
+*
+* Metadata
+* Written by    : Nathanaël Esnault
+* Verified by   : Nathanaël Esnault
+* Creation date : 2022-04-01
+* Version       : 0.1 (finished on 2021-..-..)
+* Modifications :
+* Known bugs    :
+*
+*
+* Possible Improvements
+*
+*
+* Notes
+*
+*
+* Ressources (Boards + Libraries Manager)
+* Arduino Mega 2556
+*
+* TODO
+*
+*
+* PUMP = stepper = pump = peristaltic 
+* ========================================
+*/
+
+// -------------------------- Includes[] --------------------------
+
+
+// -------------------------- Defines and Const []--------------------------
+
+// RGB LED for colorimeter
+//-------------------------
+
+#define USE_RGB_LED
+const uint8_t LED_RED_PIN   = 46; // Digital output 5V - PWM capable
+const uint8_t LED_GRN_PIN   = 44; // Digital output 5V - PWM capable
+const uint8_t LED_BLU_PIN   = 45; // Digital output 5V - PWM capable
+
+
+// -------------------------- Macros [0] --------------------------
+
+
+
+
+
+
+
+// -------------------------- Global variables []----------------
+
+
+
+
+// END OF THE FILE
